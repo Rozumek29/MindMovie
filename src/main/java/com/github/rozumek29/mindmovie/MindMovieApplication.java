51709ebedad6123a2b586dc9840775f053bac07a
Rozumek29 <wyrozumialskikacper@gmail.com>
2022-01-03 23:05:32 +0100

    Initial commit

---
+++ src/main/java/com/github/rozumek29/mindmovie/MindMovieApplication.java
@@ -0,0 +1,13 @@
+package com.github.rozumek29.mindmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindMovieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/rozumek29/mindmovie/MindMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rozumek29.mindmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindMovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
